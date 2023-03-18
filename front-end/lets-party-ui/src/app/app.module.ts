ef7f930b7b1b7c8338795591d2ac50749691e904
ALMLAMBO <aleksm007148@gmail.com>
2023-03-18 17:16:44 +0200

    Add User auth module

--- front-end/lets-party-ui/src/app/app.module.ts
+++ front-end/lets-party-ui/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {UserAuthModule} from "./user-auth/user-auth.module";
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { HttpClientModule } from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    UserAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
