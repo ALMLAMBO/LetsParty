36db434a12990245d3187bf0a846857c357b2ee6
Daniel Milchev <35624892+Daniel10106@users.noreply.github.com>
2023-03-18 15:44:35 +0200

    fixed material design

--- front-end/lets-party-ui/src/app/app.module.ts
+++ front-end/lets-party-ui/src/app/app.module.ts
@@ -3,12 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatCardModule } from '@angular/material/card';
 
 import { HomePageModule } from './home-page/home-page.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
@@ -20,9 +17,6 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserAnimationsModule,
     AppRoutingModule,
     HomePageModule,
-    MatCardModule,
-    MatSliderModule,
-    MatSlideToggleModule,
     MatButtonModule
   ],
   providers: [],
