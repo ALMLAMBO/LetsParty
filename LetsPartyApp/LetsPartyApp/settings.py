df271075f0bc9106f71ac96a19c8624925077d16
tegu1365 <tedi.d.petkova@gmail.com>
2023-03-18 13:50:49 +0200

    Party model created

--- LetsPartyApp/LetsPartyApp/settings.py
+++ LetsPartyApp/LetsPartyApp/settings.py
@@ -131,7 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+FIXTURE_DIRS = [
+    'fixtures',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
