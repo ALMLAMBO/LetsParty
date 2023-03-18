9eacb3a7b69a7ecbe45717d9ae2d29415f6687c0
Daniel Milchev <35624892+Daniel10106@users.noreply.github.com>
2023-03-19 00:58:26 +0200

    material ui for navbar and party list

--- front-end/lets-party-ui/src/app/app.module.ts
+++ front-end/lets-party-ui/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {UserAuthModule} from "./user-auth/user-auth.module";
+import { UserAuthModule } from "./user-auth/user-auth.module";
 
 import { HomePageModule } from './home-page/home-page.module';
 import { MatButtonModule } from '@angular/material/button';
