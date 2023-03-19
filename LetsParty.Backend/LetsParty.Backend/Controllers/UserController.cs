4c497334310803e8c8e84d476ec4cf37c395aa40
ALMLAMBO <aleksm007148@gmail.com>
2023-03-19 08:14:41 +0200

    Implement user controller

--- LetsParty.Backend/LetsParty.Backend/Controllers/UserController.cs
+++ LetsParty.Backend/LetsParty.Backend/Controllers/UserController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Data.Entity;
 
 namespace LetsParty.Backend.Controllers {
-	[Route("api/[controller]")]
+	[Route("api/[controller]/")]
 	[ApiController]
 	public class UserController : ControllerBase {
 		private readonly LetsPartyDbContext _context;
@@ -12,9 +12,60 @@ namespace LetsParty.Backend.Controllers {
 			_context = context;
 		}
 		
-		[HttpGet]
+		[HttpGet("all")]
 		public ActionResult<List<Models.User>> Get() {
 			return _context.Users.ToList();	
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<Models.User> GetUser(int id) {
+			return _context.Users
+				.Where(x => x.Id == id)
+				.FirstOrDefault();
+		}
+
+		[HttpPost("add")]
+		public ActionResult CreateUser([FromBody] Models.User user) {
+			if(!ModelState.IsValid) {
+				return BadRequest();
+			}
+
+			_context.Users.Add(user);
+			_context.SaveChanges();
+
+			return Ok();
+		}
+
+		[HttpPut("{id}/update")]
+		public ActionResult<Models.User> Update([FromBody] Models.User user, int id) {
+			if(!ModelState.IsValid) {
+				return BadRequest();
+			}
+
+			Models.User userToUpdate = _context.Users
+				.Where(x => x.Id == id)
+				.FirstOrDefault();
+
+			_context.Users.Update(user);
+			_context.SaveChanges();
+
+			return Ok();
+		}
+
+		[HttpDelete("{id}/delete")]
+		public ActionResult Delete(int id) { 
+			if(!ModelState.IsValid) {
+				return BadRequest();
+			}
+
+			Models.User user = _context.Users
+				.Where(x => x.Id == id)
+				.FirstOrDefault();
+
+			_context.Remove(user);
+			_context.SaveChanges();
+
+			return Ok();
 		}
 	}
 }
