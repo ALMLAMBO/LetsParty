8ce030615e32ed4136522d4016cc1ee37761b0b5
ALMLAMBO <aleksm007148@gmail.com>
2023-03-19 08:50:47 +0200

    Refactor code

--- LetsParty.Backend/LetsParty.Backend/Services/User/IUserService.cs
+++ LetsParty.Backend/LetsParty.Backend/Services/User/IUserService.cs
@@ -4,8 +4,8 @@ namespace LetsParty.Backend.Services.User {
 	public interface IUserService {
 		public List<Models.User> GetAll();
 		public Models.User GetUser(int id);
-		public ActionResult<Models.User> CreateUser(Models.User user);
-		public ActionResult<Models.User> UpdateUser(Models.User user);
-		public ActionResult<Models.User> DeleteUser(int id);
+		public Models.User CreateUser(Models.User user);
+		public Models.User UpdateUser(Models.User user);
+		public Models.User DeleteUser(int id);
 	}
 }
--- LetsParty.Backend/LetsParty.Backend/Services/User/UserService.cs
+++ LetsParty.Backend/LetsParty.Backend/Services/User/UserService.cs
@@ -1,27 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Cryptography;
+using System.Text;
 
 namespace LetsParty.Backend.Services.User {
 	public class UserService : IUserService {
-		
-		
-		public ActionResult<Models.User> CreateUser(Models.User user) {
-			throw new NotImplementedException();
+		private readonly LetsPartyDbContext _context;
+
+		public UserService(LetsPartyDbContext context) {
+			_context = context;
 		}
 
-		public ActionResult<Models.User> DeleteUser(int id) {
-			throw new NotImplementedException();
+		public Models.User CreateUser(Models.User user) {
+			HMACSHA512 hmac = new HMACSHA512();
+			byte[] key = hmac.Key;
+			user.Password = Encoding.UTF8.GetString(
+				hmac.ComputeHash(Encoding.UTF8.GetBytes(user.Password)));
+
+			_context.Users.Add(user);
+			_context.SaveChanges();
+
+			return user;
+		}
+
+		public Models.User DeleteUser(int id) {
+			Models.User user = _context.Users
+				.Where(x => x.Id == id)
+				.FirstOrDefault();
+
+			_context.Remove(user);
+			_context.SaveChanges();
+
+			return user;
 		}
 
 		public List<Models.User> GetAll() {
-			throw new NotImplementedException();
+			return _context.Users.ToList();
 		}
 
 		public Models.User GetUser(int id) {
-			throw new NotImplementedException();
+			return _context.Users
+				.Where(x => x.Id == id)
+				.FirstOrDefault();
 		}
 
-		public ActionResult<Models.User> UpdateUser(Models.User user) {
-			throw new NotImplementedException();
+		public Models.User UpdateUser(Models.User user) {
+			Models.User userToUpdate = _context.Users
+				.Where(x => x.Id == user.Id)
+				.FirstOrDefault();
+
+			userToUpdate = user;
+
+			_context.Users.Update(userToUpdate);
+			_context.SaveChanges();
+
+			return user;
 		}
 	}
 }
