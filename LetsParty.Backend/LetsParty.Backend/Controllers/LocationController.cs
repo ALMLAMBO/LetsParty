a662a25d967e772d9f215a2bebbb917ea81ecacb
tegu1365 <tedi.d.petkova@gmail.com>
2023-03-19 12:24:04 +0200

    Location Controller

---
+++ LetsParty.Backend/LetsParty.Backend/Controllers/LocationController.cs
@@ -0,0 +1,48 @@
+﻿using LetsParty.Backend.Services.Location;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LetsParty.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly ILocationService locationService;
+
+        public LocationController(ILocationService locationService)
+        {
+            this.locationService = locationService;
+        }
+
+        [HttpGet("all")]
+        public ActionResult<List<Models.Location>> GetAllLocation()
+        {
+            return locationService.All();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Models.Location> GetLocation(int id)
+        {
+            return locationService.Get(id);
+        }
+
+        [HttpPost("create-location")]
+        public ActionResult<Models.Location> CreateLocation([FromBody] Models.Location location)
+        {
+            return locationService.Create(location);
+        }
+
+        [HttpPut("{id}/update")]
+        public ActionResult<Models.Location> UpdateLocation(int id, [FromBody] Models.Location location)
+        {
+            return locationService.Update(location);
+        }
+
+        [HttpDelete("{id}/delete")]
+        public ActionResult<Models.Location> DeleteLocation(int id)
+        {
+            return locationService.Delete(id);
+        }
+    }
+}
